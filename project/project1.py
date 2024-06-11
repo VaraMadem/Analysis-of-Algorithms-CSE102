46514724c54ddc41eab9663e6fe5da3abab27178
Vara Madem <vmadem@ucsc.edu>
2024-06-11 13:18:48 -0700

    Update project1.py

--- project/project1.py
+++ project/project1.py
@@ -1,6 +1,5 @@
 import sys
 
-
 class UnionFind:
     def __init__(self, size):
         self.root = list(range(size))
@@ -26,8 +25,7 @@ class UnionFind:
 def kruskal(n, edges):
     uf = UnionFind(n)
     mst = []
-    edges.sort(key=lambda e: e[2])  
-    # sorting edges by weight
+    edges.sort(key=lambda e: e[2])
     for u, v, weight, label in edges:
         if uf.find(u) != uf.find(v):
             uf.union(u, v)
@@ -43,17 +41,14 @@ def read_graph(input_file):
         edges = []
         for i in range(m):
             u, v, w = map(int, file.readline().strip().split())
-            edges.append((u-1, v-1, w, i+1))  
-            # including zero-indexed and label
+            edges.append((u-1, v-1, w, i+1))
     return n, edges
 
 def write_output(output_file, mst):
     with open(output_file, 'w') as file:
         total_weight = sum([w for _, _, w, _ in mst])
-        for u, v, weight, label in sorted(mst, key=lambda x: x[3]):  
-            # sorting by label for consistent order
-            file.write(f'{label:>4}: ({u+1}, {v+1}) {weight:.1f}\n')  
-            # converting back to one-indexed
+        for u, v, weight, label in sorted(mst, key=lambda x: x[3]):
+            file.write(f'{label:>4}: ({u+1}, {v+1}) {weight:.1f}\n')
         file.write(f'Total Weight = {total_weight:.2f}\n')
 
 def main(input_file, output_file):
@@ -63,7 +58,7 @@ def main(input_file, output_file):
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: ./MWST <input_file> <output_file>")
+        print("Usage: MWST <input_file> <output_file>")
         sys.exit(1)
     input_file = sys.argv[1]
     output_file = sys.argv[2]
